27e3f9d08e33e60525ba0da4e2e7311a708efee2
IsraelCea <ivcea@miuandes.cl>
2019-04-10 15:03:14 -0400

    a y b listas

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:destroy,:create,:update]
+  resources :events, only: [:destroy,:create,:update]
+  resources :event_venues, only: [:destroy,:create,:update]
+  resources :tickets, only: [:destroy,:create,:update]
+  resources :ticket_types, only: [:destroy,:create,:update]
 end
+
